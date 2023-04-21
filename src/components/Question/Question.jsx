9010c316fb8875a379f816845ac264f0a7cb655f
jitunmohajan <jitunmohajan@gmail.com>
2023-04-21 20:08:20 +0600

    added show button

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Option from '../Option/Option';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+
+
 const Question = ({question,questionNumber}) => {
     const options=question.options;
-    const [answer,setAnswer] = useState(false);
+    const [showAnswer,setShowAnswer] = useState(true);
 
     const checkAnswer = (e) =>{
         console.log(e.target.value);
@@ -25,13 +31,20 @@ const Question = ({question,questionNumber}) => {
 
             <Card className='mt-5 mx-auto' style={{ width: '80%' }}>
             <ToastContainer />
-                <h4 className='ms-3'>{questionNumber} . {question.question}</h4>
+                <div className='d-flex align-items-center justify-content-between p-3'>
+                    <h4 className='ms-3'>{questionNumber} . {question.question}</h4>
+                    <FontAwesomeIcon onClick={()=>setShowAnswer(!showAnswer)}  icon={ showAnswer ? faEye: faEyeSlash } />
+                </div>
                 <ListGroup variant="flush">
                     {
                         options.map((option,indx) => <Option key={indx} option={option} optionNumber={indx} questionNumber={questionNumber} checkAnswer={checkAnswer}></Option> )
                     }
                     
+                    {
+                        showAnswer? '' : <div className='ps-3 text-success' ><p>Answer: {question.correctAnswer}</p></div>
+                    }
                 </ListGroup>
+
             </Card>
         </div>
     );
