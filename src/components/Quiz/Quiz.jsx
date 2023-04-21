ea050c5c3cfca92040229d006a54ae24cafaa66a
jitunmohajan <jitunmohajan@gmail.com>
2023-04-22 00:27:27 +0600

    update final

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import { useLoaderData } from 'react-router-dom';
 import Question from '../Question/Question';
+import './Quiz.css';
+
 const Quiz = () => {
     const quiz = useLoaderData().data;
-    console.log(quiz.questions.length);
+    console.log(quiz);
     return (
         <div>
+            <div className='quiz-title d-flex justify-content-center mt-3'>
+                <img src={quiz.logo} alt="" />
+                <h1>{quiz.name}</h1>
+            </div>
            {
                 quiz.questions.map((question,indx) => <Question key={question.id} question={question} questionNumber={indx+1}></Question> )
            } 
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -3,13 +3,13 @@ import { useLoaderData } from 'react-router-dom';
 import TopicCard from '../TopicCard/TopicCard';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
+import './Topics.css'
 const Topics = () => {
     const topics = useLoaderData().data;
     return (
 
         <div>
-            <h2 className='ms-5 mt-5 mx-auto'>Programming Topics</h2>
+            <h2 className='ms-5 mt-5 mx-auto'><span className='orange'>Programming</span> <span className='text-primary'>Topics</span></h2>
             <Row xs={1} md={2} lg={4} className="mt-5 justify-content-center">
            
                     {
