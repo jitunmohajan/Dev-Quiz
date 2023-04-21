78058b5a72df25d09f9884a21584e4cc16a779b7
jitunmohajan <jitunmohajan@gmail.com>
2023-04-21 19:26:49 +0600

    update with toaster

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Option from '../Option/Option';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Question = ({question,questionNumber}) => {
     const options=question.options;
     const [answer,setAnswer] = useState(false);
@@ -9,15 +12,19 @@ const Question = ({question,questionNumber}) => {
     const checkAnswer = (e) =>{
         console.log(e.target.value);
         if(e.target.value === question.correctAnswer){
-            alert('correct answer');
+            toast("Correct Answer !");
         }else{
-            alert('wrong answer');
+            toast('Wrong answer');
         }
     }
 
     return (
         <div>
+
+           
+
             <Card className='mt-5 mx-auto' style={{ width: '80%' }}>
+            <ToastContainer />
                 <h4 className='ms-3'>{questionNumber} . {question.question}</h4>
                 <ListGroup variant="flush">
                     {
